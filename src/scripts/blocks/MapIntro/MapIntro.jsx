a41fb2e8d579b566497301841f0cc24aed3d5b76
Vico Erv <vicoerv@gmail.com>
2024-03-27 18:55:57 +0700

    init MapIntro

---
+++ src/scripts/blocks/MapIntro/MapIntro.jsx
@@ -0,0 +1,26 @@
+import './MapIntro.scss'
+
+const MapIntro = () => {
+    return (
+        <section className="map-intro">
+            <div className="container">
+                <div className="image-wrapper">
+                    <img src="https://picsum.photos/976/530" alt="" />
+                </div>
+
+                <div className="content-wrapper">
+                    <p className="text-category">
+                        Lorem Ipsum
+                    </p>
+
+                    <h2>
+                        Designed For<br />
+                        The Five Senses
+                    </h2>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default MapIntro
---
+++ src/scripts/blocks/MapIntro/MapIntro.script.js
@@ -0,0 +1,23 @@
+const blockData = {
+    title: "MapIntro",
+    icon: "format-image",
+    category: "common",
+    attributes: {
+        title: {
+            type: 'string',
+            default: 'Title'
+        },
+        category: {
+            type: 'string',
+            default: 'Category'
+        },
+        image: {
+            type: 'object',
+            default: null
+        },
+    }
+}
+
+export {
+    blockData
+}
