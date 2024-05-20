28a8c989a48f8b5d4fd818c25a38687fb671245b
Vico Erv <vicoerv@gmail.com>
2024-05-20 10:41:02 +0700

    related post and responsive

--- single-solutions.php
+++ single-solutions.php
@@ -4,4 +4,10 @@ use Timber\Timber;
 
 $context = Timber::context();
 
+$context['relatedPosts'] = Timber::get_posts([
+    'posts_per_page' => 3,
+    'post_type' => 'solutions',
+    'post__not_in' => [$context['post']->ID],
+]);
+
 Timber::render('solution.twig', $context);
