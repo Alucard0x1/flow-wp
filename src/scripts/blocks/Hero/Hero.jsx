8ee431694753e4db88f7b4f6c1009d85777832f9
Vico Erv <vicoerv@gmail.com>
2024-05-28 12:56:32 +0700

    load immediately

--- src/scripts/blocks/Hero/Hero.jsx
+++ src/scripts/blocks/Hero/Hero.jsx
@@ -60,11 +60,11 @@ const Hero = ({ setAttributes, attributes, isSelected, edit }) => {
                 }
                 {attributes.image != null && attributes.image.type == 'video' ?
                     <>
-                        <video data-src={attributes.image.url} autoPlay loop playsInline muted className="desktop lazy"
+                        <video src={attributes.image.url} autoPlay loop playsInline muted className="desktop lazy"
                             src={edit ? attributes.image.url : null}
                         />
                         {attributes.imagePortrait != null &&
-                            <video data-src={attributes.imagePortrait.url} autoPlay loop playsInline muted className="portrait lazy"
+                            <video src={attributes.imagePortrait.url} autoPlay loop playsInline muted className="portrait lazy"
                                 src={edit ? attributes.imagePortrait.url : null}
                             />
                         }
